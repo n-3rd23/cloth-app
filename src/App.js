27bce67667f0ca63cb5aa767170658eda2b9c5bd
n-3rd23 <alansaji23@hotmail.com>
2020-05-11 22:58:44 +0530

    created collections preview

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import {Route} from 'react-router-dom'
+import ShopPage from './pages/shoppage/shop-page.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/shop' component={ShopPage}/>
     </div>
   );
 }
